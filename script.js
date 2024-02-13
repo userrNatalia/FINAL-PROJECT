97392c5db6d994ed6a62871418fb9d835ec15a79
Natalia <nataliakagwi852@gmail.com>
2024-02-13 20:05:34 +0300

    Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const   body = document.querySelector("body"),
+        sidebar = body.querySelector(".sidebar"),
+        toggle = body.querySelector(".toggle"),
+        searchBtn= body.querySelector(".serach-box");
